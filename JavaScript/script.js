7a86a327d0f38db1c0b17ce7fe825d5895d34ec4
fffppp-pixel <francype06@gmail.com>
2024-03-11 00:25:49 +0100

    map

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,6 +1,10 @@
-let arrM = ["M", "I", "B"];
+let arrM = ["Matematica", "Italiano", "Scienze"];
+let arrL = ["#","#","#"];
+
 const mainBox = document.getElementById("mainBox")
 const bar = document.getElementById("sideBar")
+
+bar.style.gridTemplateRows = `repeat( ${arrM.length}, 50px)`
 
 bar.addEventListener("mouseenter", () => {
     bar.style.width = "250px"
@@ -12,6 +16,21 @@ bar.addEventListener("mouseleave", () => {
 
 for (let i = 0; i < arrM.length; i++) {
     var elm = document.createElement("div")
-    elm.innerHTML = arrM[i]
+    var svgBox = document.createElement("div")
+    var txt = document.createElement("div")
+    var link = document.createElement("a")
+
+    link.href=arrL[i]
+
+    svgBox.innerHTML = document.getElementById(`sub_${i}`).outerHTML
+    svgBox.classList.add("svgBox")
+    elm.classList.add("navItem")
+    elm.appendChild(svgBox)
+
+    txt.innerHTML += arrM[i]
+    txt.classList.add("bTxt")
+
+    link.appendChild(txt)
+    elm.appendChild(link)
     bar.appendChild(elm)
 }
